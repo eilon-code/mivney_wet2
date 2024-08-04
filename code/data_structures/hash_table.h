7eb9ee550487e14f1fc539be5156cbc16b001603
Maxim <maximgurwitz@campus.technion.ac.il>
2024-08-05 01:36:19 +0300

    added statusType outputs and run a few more tests with valgrind

--- code/data_structures/hash_table.h
+++ code/data_structures/hash_table.h
@@ -30,6 +30,7 @@ HashTable<T>::~HashTable(){
 
 template<typename T>
 output_t<T*> HashTable<T>::get(int key) const{
+    if(key < 0)return StatusType::FAILURE;
     int index = hash(key);
     typename List<T>::Node *current = m_table[index].firstNode();
     while(current != nullptr){
@@ -43,16 +44,24 @@ output_t<T*> HashTable<T>::get(int key) const{
 
 template<typename T>
 StatusType HashTable<T>::insert(int key, const T& value){
+    if(key < 0)return StatusType::FAILURE;
     if(get(key).status()==StatusType::SUCCESS){
         return StatusType::FAILURE;
     }
     if(m_size == m_buckets){
-        resize();
+        try{
+            resize();
+        } catch(...){
+            return StatusType::ALLOCATION_ERROR;
+        }
     }
     int index = hash(key);
-    m_table[index].add(value);
-    m_size++;
-    return StatusType::SUCCESS;
+    output_t result = m_table[index].add(value);
+    if(result.status() == StatusType::SUCCESS){
+        m_size++;
+        return StatusType::SUCCESS;
+    }
+    return result.status();
 }
 
 template <typename T>
