57bc9c3010bd6494353676b07a3113dcea8691a2
eilon-code <eilon.halevy12@gmail.com>
2024-08-05 15:28:59 +0300

    small modification

--- Tests/generate_tests.py
+++ Tests/generate_tests.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import input_generate
 import ocean
 
@@ -10,12 +11,17 @@ os.makedirs(output_path, exist_ok=True)
 os.makedirs(input_path, exist_ok=True)
 
 
-test_count = 100
-line_count = 100000
+test_count = 30
+line_count = 5000
 digits = len(str(test_count))
 for i in range(1, test_count + 1):
     input_file = os.path.join(input_path, f"input{i:0{digits}d}.txt")
     output_file = os.path.join(output_path, f"output{i:0{digits}d}.txt")
-    with open(input_file, "w") as f:
-        f.write(input_generate.generate(line_count))
+    if i!=1:
+        with open(input_file, "w") as f:
+            f.write(input_generate.generate(line_count))
+    else:
+        source_file = "../test/randtest0.in"  # The file you want to copy
+        with open(source_file, "r") as src, open(input_file, "w") as dst:
+            shutil.copyfileobj(src, dst)
     ocean.main(input_file, output_file)
