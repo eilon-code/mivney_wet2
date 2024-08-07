78ac5f22dd86e0fb67df31d83815c48cb322ba18
eilon-code <eilon.halevy12@gmail.com>
2024-08-07 03:20:05 +0300

    eaiser to run tests

--- test_new.sh
+++ test_new.sh
@@ -2,7 +2,9 @@
 
 cd data_structures_wet2_tests/Tests
 
-./../../make_zip.sh wet2out.zip wet2util.h main24b2.cpp ../code/
+chmod +x compile.sh new.sh run.sh
+dos2unix compile.sh new.sh run.sh
+./../../make_zip.sh wet2out.zip wet2util.h main24b2.cpp ../../code/
 
 ./new.sh
 
