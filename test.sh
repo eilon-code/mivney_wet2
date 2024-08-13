313773095846866638e463ce0be18b8c6b0fe4ad
eilon-code <eilon.halevy12@gmail.com>
2024-08-14 02:32:15 +0300

    last fix

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+./make_zip.sh wet2out.zip wet2util.h main24b2.cpp code/
+
 cd data_structures_wet2_tests/Tests
 
 chmod +x compile.sh new.sh run.sh
 dos2unix compile.sh new.sh run.sh
-./../../make_zip.sh wet2out.zip wet2util.h main24b2.cpp ../../code/
 
-./compile.sh wet2out.zip
+./compile.sh ./../../wet2out.zip
 
 ./run.sh
--- test_new.sh
+++ test_new.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+./make_zip.sh wet2out.zip wet2util.h main24b2.cpp code/
+
 cd data_structures_wet2_tests/Tests
 
 chmod +x compile.sh new.sh run.sh
 dos2unix compile.sh new.sh run.sh
-./../../make_zip.sh wet2out.zip wet2util.h main24b2.cpp ../../code/
 
 ./new.sh
 
-./compile.sh wet2out.zip
+./compile.sh ./../../wet2out.zip
 
 ./run.sh
