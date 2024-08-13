1d46b502e498f9e6b6515064e2897e878118f929
eilon-code <eilon.halevy12@gmail.com>
2024-08-14 02:40:40 +0300

    verify submission

--- verify.sh
+++ verify.sh
@@ -2,4 +2,5 @@
 
 ./make_zip.sh wet2out.zip wet2util.h main24b2.cpp code/
 
-python3 "wet-verify.py" wet2out.zip
+python2 "wet-verify.py" wet2out.zip
+python2 "wet-verify.py" "322207671-328137831".zip
